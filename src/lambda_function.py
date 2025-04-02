d6b93308543244253f0473140ea7eeb5006115d2
Matthew Xie <mattx180@gmail.com>
2025-04-01 22:24:32 -0400

    set up lambda deployment

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,4 +3,4 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    return {"statusCode": 200, "body": json.dumps("Hello from Github!")}
