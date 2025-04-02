ca49c244645202de7ac27ddea64720a1abb89b63
Matthew Xie <mattx180@gmail.com>
2025-04-01 23:23:46 -0400

    update to include openai api key

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,9 @@
 import json
+import os
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
 def lambda_handler(event, context):
-    # TODO implement
+    print(f"Using OpenAI API Key: {openai_api_key[:5]}...")
     return {"statusCode": 200, "body": json.dumps("Hello from Github!")}
